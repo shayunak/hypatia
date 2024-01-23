fc0ed39bbf3f76045e3bef66dfe46b12ca4df7f5
hamididehshali.1 <hamididehshali.1@cse-dnc197056d.coeit.osu.edu>
2024-01-23 14:32:52 -0500

    Added apt dependency files

---
+++ normal_dependencies.sh
@@ -0,0 +1,9 @@
+pip install numpy astropy ephem networkx sgp4 geopy matplotlib statsmodels || exit 1
+pip install git+https://github.com/snkas/exputilpy.git@v1.6 || exit 1
+pip install cartopy || exit 1
+pip install numpy statsmodels || exit 1
+pip install git+https://github.com/snkas/exputilpy.git@v1.6 || exit 1
+git submodule update --init --recursive || exit 1
+pip install numpy || exit 1
+pip install git+https://github.com/snkas/exputilpy.git@v1.6 || exit 1
+pip install git+https://github.com/snkas/networkload.git@v1.3 || exit 1
---
+++ sudo_dependencies.sh
@@ -0,0 +1,4 @@
+sudo apt-get update || exit 1
+sudo apt-get install libproj-dev proj-data proj-bin libgeos-dev || exit 1
+sudo apt-get install openmpi-bin openmpi-common openmpi-doc libopenmpi-dev lcov gnuplot || exit 1
+sudo apt-get install gnuplot
